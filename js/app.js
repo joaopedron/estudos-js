b91feadfdb2c05827a0dd51127032564c2383b42
Pedro <joaopedro@alternativasv.com.br>
2016-03-15 16:43:21 -0300

    atualizacao do arquivo app.js

--- js/app.js
+++ js/app.js
@@ -8,27 +8,20 @@ var carro = {
     quantidadePessoas: 0
 };
 
-carro.mudarCor = function (color) {
-    carro.cor = color;
-};
-
 carro.adicionarPessoas = function (numeroPessoas) {
     var total = carro.quantidadePessoas + numeroPessoas;
+    var vagasRestantes = carro.capacidade - carro.quantidadePessoas;
+    var wordOne = vagasRestantes === 1 ? 'pessoa' : 'pessoas';
+    var wordTwo = vagasRestantes === 1 ? 'cabe' : 'cabem';
+
+    if (carro.quantidadePessoas === carro.capacidade && total >= carro.capacidade) {
+        return "O carro já esta lotado!";
+    }
+
+    if (numeroPessoas > carro.capacidade) {
+        return "Só " + wordTwo + " mais: " +  vagasRestantes + " " + wordOne + " no carro!";
+    }
 
     carro.quantidadePessoas += numeroPessoas;
     return 'Já temos ' + carro.quantidadePessoas + ' pessoas no carro!';
 };
-
-carro.obterCor = function () {
-    return 'A cor do carro é: ' + carro.cor;
-};
-
-
-carro.adicionarPessoas(2);
-carro.adicionarPessoas(1);
-
-carro.mudarCor('preto');
-
-console.log(carro.obterCor());
-
-console.log(carro.adicionarPessoas(2));
