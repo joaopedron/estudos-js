6439411fec7ae12e917366e6c9db42b15217cfe9
Pedro <joaopedro@alternativasv.com.br>
2016-03-15 17:39:15 -0300

    add constructor

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
+/*
+    Objetos construtores devem iniciar com a primeira letra em maiuscula
+    Não devem utilizar a sintaxe literal
+    devem utilizar o operador this para fazer referencia ao Objeto Carros
+    atribuir um valor a propriedade tem que ser com o sinal de equal [=]
+    valore sempre separados por virgula.
+
+*/
+
+function Carros (marca, modelo, ano, cor, capacidade, portas, totalPessoas) {
+    this.marca = marca,
+    this.modelo = modelo,
+    this.ano = ano,
+    this.cor = cor,
+    this.capacidade = capacidade,
+    this.portas = portas,
+    this.totalPessoas = totalPessoas
+}
+
+var carro1 = new Carros('Volkswagen', 'G5', 2014, 'Branca', 5, 4, 0);
+var carro2 = new Carros('Hyundai', 'hb20', 2015, 'Prata', 5, 4, 0);
+var carro3 = new Carros('Mitsubishi', 'Pajero', 2012, 'Preta', 7, 4, 0);
+
+/*
+    sintaxe literal
+*/
 var carro = {
     marca: 'Volkswagen',
     modelo: 'Gol g5',
@@ -26,9 +52,6 @@ carro.adicionarPessoas = function (numeroPessoas) {
     return 'Já temos ' + carro.quantidadePessoas + ' pessoas no carro!';
 };
 
-
-console.log(carro.adicionarPessoas(3));
-
 carro.removerPessoas = function (numeroPessoas) {
     var totalPessoas = carro.quantidadePessoas - numeroPessoas;
     var wordOne = totalPessoas === 1 ? 'pessoa' : 'pessoas';
@@ -36,5 +59,3 @@ carro.removerPessoas = function (numeroPessoas) {
     carro.quantidadePessoas -= numeroPessoas;
     return "foi removido " + carro.quantidadePessoas + " " + wordOne + "!";
 };
-
-console.log(carro.removerPessoas(2));
