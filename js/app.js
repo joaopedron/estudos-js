feaace44b317c3f8ca6023efb3f7a4bdb2d2b99c
Pedro <joaopedro@alternativasv.com.br>
2016-03-15 14:55:29 -0300

    update estudos-js

--- js/app.js
+++ js/app.js
@@ -1,21 +1,34 @@
-Number.prototype.formatMoney = function(c, d, t){
-   
-   var n = this;
-   var c = isNaN(c = Math.abs(c)) ? 2 : c; 
-   var d = d == undefined ? "," : d;
-   var t = t == undefined ? "." : t;
-   var s = n < 0 ? "-" : "";
-   var i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + ""; 
-   var j = (j = i.length) > 3 ? j % 3 : 0;
-   
-   return s 
-          + (j ? i.substr(0, j) + t : "") 
-          + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) 
-          + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+var carro = {
+    marca: 'Volkswagen',
+    modelo: 'Gol g5',
+    ano: 2013,
+    cor: 'Prata',
+    portas: 4,
+    capacidade: 5,
+    quantidadePessoas: 0
 };
 
-var x = (2000).formatMoney(/*'casas decimais', 'separador virgula', 'separador ponto'*/);
+carro.mudarCor = function (color) {
+    carro.cor = color;
+};
 
-console.log(x);
+carro.adicionarPessoas = function (numeroPessoas) {
+    var total = carro.quantidadePessoas + numeroPessoas;
 
-// R$: 2.000,00 =D
+    carro.quantidadePessoas += numeroPessoas;
+    return 'Já temos ' + carro.quantidadePessoas + ' pessoas no carro!';
+};
+
+carro.obterCor = function () {
+    return 'A cor do carro é: ' + carro.cor;
+};
+
+
+carro.adicionarPessoas(2);
+carro.adicionarPessoas(1);
+
+carro.mudarCor('preto');
+
+console.log(carro.obterCor());
+
+console.log(carro.adicionarPessoas(2));
